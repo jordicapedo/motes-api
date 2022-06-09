a9f559db738639e9612a9df674e855e327437e01
kapatiuska <65649227+jordicapedo@users.noreply.github.com>
2022-06-09 13:03:22 +0200

    first commit

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+const options = {
+  arrowParens: 'avoid',
+  singleQuote: true,
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  semi: false,
+  tabWidth: 2,
+  trailingComma: 'none',
+  useTabs: true
+}
+
+module.exports = options
