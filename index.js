aa9c308ab33e08a720ef232fc23ceac3e6a9908e
kapatiuska <65649227+jordicapedo@users.noreply.github.com>
2022-06-10 17:18:57 +0200

    add heroku config

--- index.js
+++ index.js
@@ -79,7 +79,8 @@ app.use((request, response) => {
   response.status(404).json({ error: 'Not found' })
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
+
 app.listen(PORT, () => {
   console.log(`Server running at port ${PORT}`)
 })
