8b01b80c90f42f1f4260c9989d77520b1b08c8ea
kapatiuska <65649227+jordicapedo@users.noreply.github.com>
2022-07-05 13:50:49 +0200

    model user, add test

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ const Sentry = require('@sentry/node')
 const Tracing = require('@sentry/tracing')
 const express = require('express')
 const cors = require('cors')
-const Note = require('./models/Note')
 
 const app = express()
 const logger = require('./loggerMiddleware')
 const notFound = require('./middleware/notFound')
 const handleErrors = require('./middleware/handleErrors')
+const usersRouter = require('./controllers/users')
+const notesRouter = require('./controllers/notes')
 
 app.use(cors())
 app.use(express.json())
@@ -43,74 +44,9 @@ app.get('/', (request, response) => {
   response.send('<h1>This is the Motes API 🚀 </h1>')
 })
 
-// obtenemos todas las notas
-app.get('/api/notes', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
-  })
-})
+app.use('/api/notes', notesRouter)
+app.use('/api/users', usersRouter)
 
-// obtener una nota por id
-app.get('/api/notes/:id', (request, response, next) => {
-  const { id } = request.params
-
-  Note.findById(id)
-    .then(note => {
-      if (note) return response.json(note)
-      response.status(404).end()
-    })
-    .catch(next)
-})
-
-// actualizar una nota
-app.put('/api/notes/:id', (request, response, next) => {
-  const { id } = request.params
-  const note = request.body
-
-  const newNowInfo = {
-    content: note.content,
-    important: note.important
-  }
-
-  Note.findByIdAndUpdate(id, newNowInfo)
-    .then(result => {
-      response.json(result)
-    })
-    .catch(error => next(error))
-})
-
-// eliminar una nota
-app.delete('/api/notes/:id', (request, response, next) => {
-  const { id } = request.params
-  console.log(id)
-  Note.findByIdAndDelete(id)
-    .then(() => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
-})
-
-// crear una nota
-app.post('/api/notes', (request, response, next) => {
-  const note = request.body
-
-  if (!note || !note.content) {
-    return response.status(400).json({ error: 'content missing' })
-  }
-
-  const newNote = new Note({
-    content: note.content,
-    date: new Date(),
-    important: note.important || false
-  })
-
-  newNote
-    .save()
-    .then(savedNote => {
-      response.json(savedNote)
-    })
-    .catch(error => next(error))
-})
 app.use(notFound)
 // The error handler must be before any other error middleware and after all controllers
 app.use(Sentry.Handlers.errorHandler())
@@ -118,7 +54,8 @@ app.use(Sentry.Handlers.errorHandler())
 app.use(handleErrors)
 
 const PORT = process.env.PORT
-
-app.listen(PORT, () => {
-  console.log(`Server running at port ${PORT}`)
+const server = app.listen(PORT, () => {
+  console.log(`Server running at port http://localhost:${PORT}`)
 })
+
+module.exports = { app, server }
